3486f25036185a22fbade6ffafef57783a014a93
Mattias Eklund <mattias.eklund@ockelbook.se>
2025-11-18 14:24:02 +0100

    Uppdaterar exempel 3

--- lektion10/js/example.js
+++ lektion10/js/example.js
@@ -11,3 +11,19 @@ let newItemLast = document.createElement("li");
 let newTextLast = document.createTextNode("cream");
 newItemLast.appendChild(newTextLast);
 list.appendChild(newItemLast);
+
+// alert("Hej på er");
+
+// Byta färg för alla li element att använda i .cool klass
+let listItems = document.querySelectorAll("li");
+for (let i = 0; i < listItems.length;  i++) { 
+    listItems[i].className = "cool"; 
+
+}
+
+// Lägga till räknare i header h2
+let heading = document.querySelector("h2");
+let headingText = heading.firstChild.nodeValue;
+let totalItems = listItems.length;
+let newHeading = headingText + "<span>" + totalItems + "</span>";
+heading.innerHTML = newHeading;
