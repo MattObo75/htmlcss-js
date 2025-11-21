decbdf4475723110d87115139cee4f16bd7a2613
Mattias Eklund <mattias.eklund@ockelbook.se>
2025-11-21 13:58:10 +0100

    Uppdaterar Exempel 7

--- lektion11/Example 7_BasicGoogleMapsAPI/js/google-map.js
+++ lektion11/Example 7_BasicGoogleMapsAPI/js/google-map.js
@@ -1,5 +1,16 @@
 function init() {
     let mapOptions = {
-        center = new google.maps.LatLng(40.782710,)
+        center = new google.maps.LatLng(40.782710, -73.965310),
+        myTypeId: google.maps.MyTypeId.ROADMAP,
+        zoom: 10
+    }
+    let venueMap;
+    venueMap = new google.maps.Map(document.getElementById("map"),mapOptions);
+
+    function loadScript() {
+        let script = document.createElement("script");
+        script.src  = http://maps.googleapis.com/maps/api/js?sensor=false&callback=init;
+        document.body.appendChild(script);
+        
     }
 }
